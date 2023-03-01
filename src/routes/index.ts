d4e467b9fd9ab7b6b90d806bb5ba3be24da5b869
doubled3264 <dede.doubled3264@gmail.com>
2023-03-01 15:19:54 +0700

    only commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,75 +1,13 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router'
 import { RouteRecordRaw } from 'vue-router'
+import publicRoute from './public'
+import privateRoute from './private'
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/home',
-  },
-  {
-    path: '/home',
-    component: () => import('../pages/Home.vue'),
-  },
-  {
-    path: '/customers',
-    component: () => import('../pages/customers/BaseCustomer.vue'),
-    children: [{
-      path: '',
-      component: () => import('../pages/customers/CustomerList.vue')
-    },
-    {
-      path: 'add',
-      component: () => import('../pages/customers/CustomerAdd.vue')
-    }]
-  },
-  {
-    path: '/orders',
-    component: () => import('../pages/Orders.vue'),
-  },
-  {
-    path: '/bills',
-    component: () => import('../pages/Bills.vue'),
-  },
-  {
-    path: '/test',
-    component: () => import('../pages/Test.vue'),
-  },
-  //
-  // {
-  //    path: '/',
-  //    redirect: '/dashboard',
-  // },
-  // {
-  //    path: '/',
-  //    component: BasePages,
-  //    children: [
-  //       {
-  //          path: '',
-  //          redirect: '/dashboard',
-  //       },
-  //       {
-  //          path: 'dashboard',
-  //          component: () => import('../pages/Dashboard.vue'),
-  //       },
-  //       {
-  //          path: 'customers',
-  //          component: () => import('../pages/Customers.vue'),
-  //       },
-  //       {
-  //          path: 'orders',
-  //          component: () => import('../pages/Orders.vue'),
-  //       },
-  //       {
-  //          path: 'Bills',
-  //          component: () => import('../pages/Bills.vue'),
-  //       },
-  //    ],
-  // },
-]
+const routes: Array<RouteRecordRaw> = [...publicRoute, ...privateRoute]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+   history: createWebHistory(),
+   routes,
 })
 
 export default router
---
+++ src/store/modules/auth.ts
@@ -0,0 +1,55 @@
+import axios from 'axios'
+
+type State = {
+   admin: object
+   token: string
+}
+
+function storeToken(token: string) {
+   localStorage.setItem('token', token)
+}
+
+function setAxiosHeader(token: string) {
+   axios.defaults.headers.common['Authorization'] = token
+}
+
+export default {
+   namespaced: true,
+   state: {
+      admin: null,
+      token: null,
+   },
+   getters: {
+      user: (state: State) => {
+         return state.admin && state.token
+      },
+   },
+   mutations: {
+      admin: (state: State, data: any) => {
+         state.admin = data
+      },
+      token: (state: State, data: any) => {
+         state.token = data
+      },
+   },
+   actions: {
+      // async login({commit}, credentials) {
+
+      // },
+      async getAccess({ commit }: any, token: string) {
+         setAxiosHeader(token)
+         return new Promise((resolve, reject) => {
+            axios
+               .get('auth/admin-info')
+               .then((response) => {
+                  commit('user', response.data.data.adminData)
+                  resolve(response)
+               })
+               .catch((err) => {
+                  console.log(err)
+                  reject(err)
+               })
+         })
+      },
+   },
+}
